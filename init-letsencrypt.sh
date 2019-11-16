d8daa7b865534b04e431d2b61eeb253f2ddaabac
alanhg <qianghe421@gmail.com>
2019-11-16 21:50:50 +0800

    fix deploy error

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(alanhe.me)
+domains=(ray.alanhe.me) # your domain name
 rsa_key_size=4096
 data_path="./certbot"
 email="qianghe421@163.com" # Adding a valid address is strongly recommended
